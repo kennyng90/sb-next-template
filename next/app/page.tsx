273f7700d96f7ae04e215132d112647010b00197
Kenny Nguyen <kennamngu@gmail.com>
2024-08-21 15:12:30 +0200

    first commit

---
+++ next/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Button } from "@ds/ui";
+
+export default function Home() {
+  return (
+    <main>
+      <Button label='hello wold' primary></Button>
+    </main>
+  );
+}
